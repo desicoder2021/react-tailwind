44c25f70d74e6d026c4590da8212f710a44185cf
desicoder2021 <77558826+desicoder2021@users.noreply.github.com>
2024-06-17 19:16:43 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ function App() {
 	return (
 		<>
 			
-<a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+<a href="#" className="">
 
-<h5 className="">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h5>
-<p className="">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed egestas nec ante mollis pretium. Fusce porta, quam quis vehicula faucibus, lectus massa malesuada urna, ut posuere elit tellus in urna</p>
+<h5 className="">Title comes here</h5>
+<p className="">Description will come here</p>
 </a>
 
 		</>
