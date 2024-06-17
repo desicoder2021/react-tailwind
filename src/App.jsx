8eb438f65e8e50a7144899f607a1d8437668a59f
desicoder2021 <77558826+desicoder2021@users.noreply.github.com>
2024-06-17 19:14:38 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ function App() {
 	return (
 		<>
 			
-<a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+<a href="#" className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
 
-<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h5>
-<p class="font-normal text-gray-700 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed egestas nec ante mollis pretium. Fusce porta, quam quis vehicula faucibus, lectus massa malesuada urna, ut posuere elit tellus in urna</p>
+<h5 className="">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h5>
+<p className="">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed egestas nec ante mollis pretium. Fusce porta, quam quis vehicula faucibus, lectus massa malesuada urna, ut posuere elit tellus in urna</p>
 </a>
 
 		</>
