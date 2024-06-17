17a034eaa376b5bf936e25ea877165a0a5bfccc9
desicoder2021 <77558826+desicoder2021@users.noreply.github.com>
2024-06-17 19:38:50 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,8 @@ import React from "react";
 function App() {
 	return (
 		<>
-			<div className="bg-gray-900 h-screen text-center flex justify-center items-center flex-col place-items-center">
-				<p className="inline-block text-5xl font-extrabold  text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 ">
-					React + Tailwind CSS
-				</p>
-			</div>
+			<span class="text-xs font-medium me-2 px-2.5 py-0.5">blue badge</span>
+
 		</>
 	);
 }
